0b9d40e37592a7b3f2902640854eeeff8e9ed2ef
NCH-01 <proficientwitch@gmail.com>
2024-12-07 17:42:31 +0700

    Squashed commit of the following:
    commit 2390ae19c1005d7ea42e9afd2ce7f0df1e3562fc
    Author: NCH-01 <proficientwitch@gmail.com>
    Date:   Sat Dec 7 17:16:36 2024 +0700
    
        u

--- Assets/_Data/Enemies/Bandit/BanditCombat.cs
+++ Assets/_Data/Enemies/Bandit/BanditCombat.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using UnityEngine;
 
 public class BanditCombat : GameMonoBehaviour
@@ -6,27 +6,53 @@ public class BanditCombat : GameMonoBehaviour
     [SerializeField] protected bool detectPlayer;
     float dir;
     Vector2 posTarget;
+    [SerializeField] float dis = 5f;
 
     protected virtual void Update()
     {
-        DetectPlayer();
+        //DetectPlayer();
+        SweepRaycasts(transform.parent.position, 10f,45, 10, LayerMask.GetMask("Player"));
     }
 
     private void DetectPlayer()
     {
         dir = transform.parent.localScale.x;
-        posTarget = new Vector2(dir * 5 + transform.position.x, transform.position.y);
-        //detectPlayer = Physics2D.Raycast(transform.parent.position,
-        //    new Vector2(dir, 0), posTarget.x, LayerMask.NameToLayer("Player"));
-        RaycastHit2D[] physics = Physics2D.RaycastAll(transform.parent.position, posTarget, posTarget.x, LayerMask.NameToLayer("Player"));
-        foreach(var tmp in physics) {
-            Debug.Log(tmp.transform.name);
+        posTarget = new Vector2(dir * dis + transform.position.x, transform.position.y);
+        detectPlayer = Physics2D.Raycast(transform.parent.position,
+            new Vector2(dir, 0), transform.parent.position.x + dis, LayerMask.GetMask("Player"));
+        //detectPlayer = Physics2D.Linecast(transform.parent.position, posTarget, LayerMask.GetMask("Player"));
+    }
+
+    private void SweepRaycasts(Vector2 origin, float range, float angle, int numRays, LayerMask targetLayer)
+    {
+        float startAngle = -angle / 2; // Góc bắt đầu (âm)
+        float angleStep = angle / (numRays - 1); // Bước giữa các tia
+
+        for (int i = 0; i < numRays; i++)
+        {
+            // Tính góc và hướng của từng ray
+            float currentAngle = startAngle + angleStep * i;
+            Vector2 direction = Quaternion.Euler(0, 0, currentAngle) * Vector2.right;
+
+            // Bắn raycast
+            RaycastHit2D hit = Physics2D.Raycast(origin, direction, range, targetLayer);
+
+            // Debug ray trong Scene View
+            Debug.DrawRay(origin, direction * range, hit.collider ? Color.red : Color.green);
+
+            // Xử lý khi raycast chạm vào Player
+            if (hit.collider && hit.collider.CompareTag("Player"))
+            {
+                Debug.Log("Detected Player at angle: " + currentAngle);
+                // Kích hoạt hành động (ví dụ: tấn công)
+            }
         }
     }
+
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.parent.position, posTarget);
+        Gizmos.DrawRay(transform.parent.position, posTarget);
     }
 }
