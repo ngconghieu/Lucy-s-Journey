58b9174c50b429e6bd51b1162fbcd7000bfe5e23
just1night <157812037+just1night@users.noreply.github.com>
2024-12-20 05:48:15 +0700

    update trap

---
+++ Assets/_Data/Scenes/_Script/Traps/Trap_behavior.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Trap_behavior : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private int damage = 1;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag=="Player")
+        {
+            //collision.gameObject.GetComponent<DmgReceiver>().Deduct(damage);
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+}
+
