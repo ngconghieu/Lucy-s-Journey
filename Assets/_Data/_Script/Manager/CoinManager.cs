4bba12fd34d48da3e2222669b8acb020f7695f75
just1night <157812037+just1night@users.noreply.github.com>
2025-01-13 22:57:45 +0700

    update result

--- Assets/_Data/_Script/Manager/CoinManager.cs
+++ Assets/_Data/_Script/Manager/CoinManager.cs
@@ -5,22 +5,27 @@ public class CoinManager : MonoBehaviour
     public static CoinManager instance;
     private int coins;
     [SerializeField] private TextMeshProUGUI coinText;
+    [SerializeField] private TextMeshProUGUI resultText;
+
 
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
+
         }
     }
 
     private void OnGUI()
     {
         coinText.text = coins.ToString();
+        resultText.text = coins.ToString();
     }
     public void ChangeCoins(int amount)
     {
         coins += amount;
         Debug.Log("Coins: " + coins);
     }
+
 }
